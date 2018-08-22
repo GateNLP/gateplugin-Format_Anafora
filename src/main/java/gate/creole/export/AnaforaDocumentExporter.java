3132081ab126f3f80a67046ba05043b336feda2b
Mark A. Greenwood <m.a.greenwood@sheffield.ac.uk>
2018-08-22 13:51:48 +0100

    more work on export support

--- src/main/java/gate/creole/export/AnaforaDocumentExporter.java
+++ src/main/java/gate/creole/export/AnaforaDocumentExporter.java
@@ -2,12 +2,21 @@ package gate.creole.export;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URL;
+import java.util.regex.Pattern;
+
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.output.XMLOutputter;
 
 import gate.Document;
 import gate.DocumentExporter;
 import gate.FeatureMap;
+import gate.creole.anafora.AnaforaSchema;
 import gate.creole.metadata.AutoInstance;
+import gate.creole.metadata.CreoleParameter;
 import gate.creole.metadata.CreoleResource;
+import gate.creole.metadata.RunTime;
 
 //@CreoleResource(name = "Anafora XML Exporter", comment = "Export documents in Anafora XML format", tool = true, autoinstances = @AutoInstance)
 public class AnaforaDocumentExporter extends DocumentExporter {
@@ -17,12 +26,51 @@ public class AnaforaDocumentExporter extends DocumentExporter {
   }
 
   private static final long serialVersionUID = -8451295843341636156L;
+  
+  private static XMLOutputter outputter = new XMLOutputter();
+  
+  @RunTime
+  @CreoleParameter
+  public void setSchemaLocation(URL schemaLocation) { }
+  public URL getSchemaLocation() { return null; }
+  
+  
+  @RunTime
+  @CreoleParameter(defaultValue=".*\\.completed")
+  public void setAnnotationSetRegexp(String regexp) { }
+  public String getAnnotationSetRegexp() { return null; }
 
   @Override
-  public void export(Document arg0, OutputStream arg1, FeatureMap arg2)
+  public void export(Document doc, OutputStream out, FeatureMap options)
       throws IOException {
-    // TODO Auto-generated method stub
 
+    if (!options.containsKey("schemaLocation"))
+      throw new IOException("Schema Location Not Specified");
+    
+    if (!options.containsKey("annotationSetRegexp"))
+      throw new IOException("No AnnotationSet Regexp specified");
+    
+    try {
+      AnaforaSchema schema = new AnaforaSchema((URL)options.get("schemaLocation"));
+      
+      Pattern setNamePattern = Pattern.compile((String)options.get("annotationSetRegexp"));
+      
+      for (String name : doc.getAnnotationSetNames()) {
+        if (setNamePattern.matcher(name).find()) {
+          System.out.println("exporting " + name);
+          
+          Element annotations = schema.getAnnotations(doc.getAnnotations(name));
+          
+          outputter.output(annotations, System.out);
+        }
+      }
+      
+      
+    } catch(JDOMException e) {
+      throw new IOException(e);
+    }
+    
+    //use one (or more) regexps to select annotation sets
   }
 
 }
